02bf29d140388a36042844ee34db590aff102a1a
antiveroemmanuel <47337966+antiveroemmanuel@users.noreply.github.com>
2019-03-23 15:44:00 -0300

    Add files via upload

---
+++ Clase_02/TP/TP01/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "operacionesmatematicas.h"
+int main(void)
+{
+    int A;
+    int B;
+
+printf("Ingrese numero x: ");
+scanf("%d",&A);
+printf("Ingrese numero y: ");
+scanf("%d",&B);
+suma(A,B);
+    return 0;
+}
+
+
+
---
+++ Clase_02/TP/TP01/operacionesmatematicas.h
@@ -0,0 +1,15 @@
+#ifndef OPERACIONESMATEMATICAS_H_INCLUDED
+#define OPERACIONESMATEMATICAS_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+
+int suma()
+{
+    int x;
+    int y;
+    int suma = x + y;
+    return suma;
+
+
+#endif // OPERACIONESMATEMATICAS_H_INCLUDED
+}
